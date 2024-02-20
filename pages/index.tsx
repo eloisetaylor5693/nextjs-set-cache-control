e5a3bd34c6799ff669cb72203c0747bfad44f95f
Eloise Taylor <eloise.taylor@futurelearn.com>
2024-02-20 17:29:42 +0000

    Set cache control using the context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
+import { GetServerSidePropsContext } from "next";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
   return null;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  context.res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=300, stale-while-revalidate=600"
+  );
+  return {
+    props: { title: "hello world" },
+  };
 }
